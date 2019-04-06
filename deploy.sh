ce96d6d504fd4d0604bc34ad1a897f50c5ff2a6f
matsushin <matsushin.dev@gmail.com>
2019-04-06 23:00:38 +0900

    設定変更

--- deploy.sh
+++ deploy.sh
@@ -59,7 +59,7 @@ make_task_def(){
 }
 
 push_ecr_image(){
-	eval $(aws ecr get-login --region ap-northeast-1)
+	eval $(aws ecr get-login --region ap-northeast-1 --no-include-email)
 	docker push $AWS_ACCOUNT_ID.dkr.ecr.ap-northeast-1.amazonaws.com/go-sample-webapp:$CIRCLE_SHA1
 }
 
