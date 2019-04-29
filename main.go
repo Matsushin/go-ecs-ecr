fa6847a818e75e784c201402518d22c2db7f3e42
matsushin <matsushin.dev@gmail.com>
2019-04-29 09:20:06 +0900

    設定変更

--- main.go
+++ main.go
@@ -1,17 +1,17 @@
 package main
 
-import(
+import (
 	"fmt"
 	"net/http"
 )
 
-func mainHandler() http.HandlerFunc{
-	return http.HandlerFunc(func(w http.ResponseWriter, r *http.Request){
-		fmt.Fprintf(w, "Hello World!")
+func mainHandler() http.HandlerFunc {
+	return http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
+		fmt.Fprintf(w, "Hello World!!")
 	})
 }
 
-func main(){
+func main() {
 	http.HandleFunc("/", mainHandler())
 	http.ListenAndServe(":8080", nil)
 }
