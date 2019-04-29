18af7a3fe864020587029ba3cdb7efac0875128a
matsushin <matsushin.dev@gmail.com>
2019-04-29 16:22:02 +0900

    強制デプロイメント実行

--- deploy2.sh
+++ deploy2.sh
@@ -2,4 +2,4 @@
 
 eval $(aws ecr get-login --no-include-email --region ap-northeast-1)
 docker push $AWS_ACCOUNT_ID.dkr.ecr.ap-northeast-1.amazonaws.com/go-sample-webapp:latest
-./scripts/ecs-deploy --cluster sample-webapp-cluster --service-name sample-webapp-service5 --image $AWS_ACCOUNT_ID.dkr.ecr.ap-northeast-1.amazonaws.com/go-sample-webapp:latest --timeout 600
+./scripts/ecs-deploy --cluster sample-webapp-cluster --service-name sample-webapp-service5 --image $AWS_ACCOUNT_ID.dkr.ecr.ap-northeast-1.amazonaws.com/go-sample-webapp:latest --timeout 600 --force-new-deployment
