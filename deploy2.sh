86e4fb85456d94e57f374f044d343694795e2c88
matsushin <matsushin.dev@gmail.com>
2019-04-29 12:05:21 +0900

    ecs-deployテスト

---
+++ deploy2.sh
@@ -0,0 +1,5 @@
+#!/bin/sh
+
+eval $(aws ecr get-login --region ap-northeast-1)
+docker push $AWS_ACCOUNT_ID.dkr.ecr.ap-northeast-1.amazonaws.com/go-sample-webapp:$CIRCLE_SHA1
+./scripts/ecs-deploy --cluster sample-webapp-cluster --service-name sample-webapp-service5 --image $AWS_ACCOUNT_ID.dkr.ecr.ap-northeast-1.amazonaws.com/go-sample-webapp:$CIRCLE_SHA1
