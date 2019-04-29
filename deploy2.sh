fee17258e8f41eff94351a9aff4ab21bee196206
matsushin <matsushin.dev@gmail.com>
2019-04-29 12:21:12 +0900

    タイムアウト時間変更

--- deploy2.sh
+++ deploy2.sh
@@ -2,4 +2,4 @@
 
 eval $(aws ecr get-login --region ap-northeast-1 --no-include-email)
 docker push $AWS_ACCOUNT_ID.dkr.ecr.ap-northeast-1.amazonaws.com/go-sample-webapp:$CIRCLE_SHA1
-./scripts/ecs-deploy --cluster sample-webapp-cluster --service-name sample-webapp-service5 --image $AWS_ACCOUNT_ID.dkr.ecr.ap-northeast-1.amazonaws.com/go-sample-webapp:$CIRCLE_SHA1
+./scripts/ecs-deploy --cluster sample-webapp-cluster --service-name sample-webapp-service5 --image $AWS_ACCOUNT_ID.dkr.ecr.ap-northeast-1.amazonaws.com/go-sample-webapp:$CIRCLE_SHA1 --timeout 600
