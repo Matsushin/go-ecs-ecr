4ea81d3d2f5338d4c730a05adf0907601c8df846
matsushin <matsushin.dev@gmail.com>
2019-04-29 14:34:40 +0900

    設定変更

--- deploy2.sh
+++ deploy2.sh
@@ -1,5 +1,5 @@
 #!/bin/sh
 
 eval $(aws ecr get-login --no-include-email --region ap-northeast-1)
-docker push $AWS_ACCOUNT_ID.dkr.ecr.ap-northeast-1.amazonaws.com/go-sample-webapp:$CIRCLE_SHA1
-./scripts/ecs-deploy --cluster sample-webapp-cluster --service-name sample-webapp-service5 --image $AWS_ACCOUNT_ID.dkr.ecr.ap-northeast-1.amazonaws.com/go-sample-webapp:$CIRCLE_SHA1 --timeout 600
+docker push $AWS_ACCOUNT_ID.dkr.ecr.ap-northeast-1.amazonaws.com/go-sample-webapp:latest
+./scripts/ecs-deploy --cluster sample-webapp-cluster --service-name sample-webapp-service5 --image $AWS_ACCOUNT_ID.dkr.ecr.ap-northeast-1.amazonaws.com/go-sample-webapp:latest --timeout 600
