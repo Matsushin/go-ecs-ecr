fc0dde72feecdfbbfad1440cb9327ab5b65ca720
matsushin <matsushin.dev@gmail.com>
2019-04-29 12:07:30 +0900

    設定変更

--- deploy2.sh
+++ deploy2.sh
@@ -1,5 +1,5 @@
 #!/bin/sh
 
-eval $(aws ecr get-login --region ap-northeast-1)
+eval $(aws ecr get-login --region ap-northeast-1 --no-include-email)
 docker push $AWS_ACCOUNT_ID.dkr.ecr.ap-northeast-1.amazonaws.com/go-sample-webapp:$CIRCLE_SHA1
 ./scripts/ecs-deploy --cluster sample-webapp-cluster --service-name sample-webapp-service5 --image $AWS_ACCOUNT_ID.dkr.ecr.ap-northeast-1.amazonaws.com/go-sample-webapp:$CIRCLE_SHA1
